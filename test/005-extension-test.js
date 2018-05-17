4202de0508d4d49a6ee8e7d792aa51c1202f4aee
Philipp Heyse <philipp.heyse@gmail.com>
2018-05-17 10:37:01 +0200

    refactor extension-test

--- test/005-extension-test.js
+++ test/005-extension-test.js
@@ -29,32 +29,37 @@ describe('Extension Action', function () {
     return tools.cleanup()
   })
 
-  it('should create default structure on extension create and install frontend dependencies', function (done) {
+  it('should create default structure on extension create and install frontend dependencies', async () => {
     const command = `${tools.getExecutable()} extension create frontend backend --extension @shopgateIntegrationTest/myAwesomExtension --trusted true`
-    const proc = exec(command)
-    const messages = []
 
-    proc.stdout.pipe(JSONStream.parse()).pipe(es.map(data => {
-      messages.push(data.msg)
-    }))
+    return new Promise((resolve, reject) => {
+      const proc = exec(command)
+      const messages = []
+      proc.stdout.pipe(JSONStream.parse()).pipe(es.map(data => {
+        messages.push(data.msg)
+      }))
 
-    proc.on('exit', (code) => {
-      console.log(messages)
-      assert.ok(messages.includes('Downloading boilerplate ... done'), 'should have downloaded boilerplate')
-      assert.ok(messages.includes('Updating backend files ... done'), 'should have updated backend files')
-      assert.ok(messages.includes('Installing frontend depenencies ...'), 'should have installed frontend dependencies')
-      assert.ok(messages.includes('Extension "@shopgateIntegrationTest/myAwesomExtension" created successfully'), 'should have logged the successful extension creation')
-      done()
+      proc.on('exit', (code) => {
+        try {
+          assert.ok(messages.includes('Downloading boilerplate ... done'), 'should have downloaded boilerplate')
+          assert.ok(messages.includes('Updating backend files ... done'), 'should have updated backend files')
+          assert.ok(messages.includes('Installing frontend depenencies ...'), 'should have installed frontend dependencies')
+          assert.ok(messages.includes('Extension "@shopgateIntegrationTest/myAwesomExtension" created successfully'), 'should have logged the successful extension creation')
+          resolve()
+        } catch (err) {
+          reject(err)
+        }
+      })
     })
   })
 
-  it('should attach extensions and apply its config', function (done) {
+  it('should attach extensions and apply its config', async () => {
     const testExtensionFolder = path.join(tools.getProjectFolder(), 'extensions', 'testing-manual')
-    const ex = async (done) => {
-      await fsEx.mkdirp(testExtensionFolder, { mode: '777' })
-      await fsEx.copy(path.join(tools.getRootDir(), 'test', 'fixtures', 'shopgate-cart'), testExtensionFolder)
-      const command = `${tools.getExecutable()} extension attach`
+    await fsEx.mkdirp(testExtensionFolder, { mode: '777' })
+    await fsEx.copy(path.join(tools.getRootDir(), 'test', 'fixtures', 'shopgate-cart'), testExtensionFolder)
+    const command = `${tools.getExecutable()} extension attach`
 
+    return new Promise((resolve, reject) => {
       const proc = exec(command)
       const messages = []
       proc.stdout.on('data', data => {
@@ -64,48 +69,50 @@ describe('Extension Action', function () {
       const appConfigJson = async () => (fsEx.readJson(path.join(tools.getProjectFolder(), 'extensions', 'testing-manual', 'extension', 'config.json')))
 
       proc.on('exit', async (code, signal) => {
-        await (new Promise(resolve => (setTimeout(resolve, 500))))
-        assert.ok(messages.includes('Attached @shopgate/cart (testing-manual)'), 'should log attachment of extension')
-
-        setTimeout(() => {
-          appConfigJson().then(appConfig => {
-            assert.equal(appConfig.cakeUrl, 'extension_config_original', 'should overwrite the extensions config.json')
-            done()
-          }).catch((err) => {
-            assert.ifError(err)
-            done()
-          })
-        }, 5000)
+        try {
+          assert.ok(messages.includes('Attached @shopgate/cart (testing-manual)'), 'should log attachment of extension')
+          const appConfig = await appConfigJson()
+          assert.equal(appConfig.cakeUrl, 'extension_config_original', 'should overwrite the extensions config.json')
+          resolve()
+        } catch (err) {
+          reject(err)
+        }
       })
-    }
-    ex(done)
+    })
   })
 
-  it('should detach extensions', function (done) {
+  it('should detach extensions', async () => {
     const testExtensionFolder = path.join(tools.getProjectFolder(), 'extensions', 'testing-manual')
-    const ex = async (done) => {
-      await fsEx.mkdirp(testExtensionFolder)
-      await fsEx.copy(path.join(tools.getRootDir(), 'test', 'fixtures', 'shopgate-cart'), testExtensionFolder)
-      const command = `${tools.getExecutable()} extension attach`
+    await fsEx.mkdirp(testExtensionFolder)
+    await fsEx.copy(path.join(tools.getRootDir(), 'test', 'fixtures', 'shopgate-cart'), testExtensionFolder)
+    const command = `${tools.getExecutable()} extension attach`
+
+    return new Promise((resolve, reject) => {
       const proc = exec(command)
       const messages = []
       proc.stdout.pipe(JSONStream.parse()).pipe(es.map(data => {
         messages.push(data.msg)
       }))
 
-      proc.on('exit', async (code, signal) => {
-        assert.ok(messages.includes('Attached @shopgate/cart (testing-manual)'), 'should log attachment of extension')
-
-        const detProc = exec(`${tools.getExecutable()} extension detach testing-manual`)
-        detProc.stdout.pipe(JSONStream.parse()).pipe(es.map(data => {
-          messages.push(data.msg)
-        }))
-        detProc.on('exit', async () => {
-          assert.ok(messages.includes('Detached @shopgate/cart'), 'should log detachment of extension')
-          done()
-        })
+      proc.on('exit', (code, signal) => {
+        try {
+          assert.ok(messages.includes('Attached @shopgate/cart (testing-manual)'), 'should log attachment of extension')
+          const detProc = exec(`${tools.getExecutable()} extension detach testing-manual`)
+          detProc.stdout.pipe(JSONStream.parse()).pipe(es.map(data => {
+            messages.push(data.msg)
+          }))
+          detProc.on('exit', async () => {
+            try {
+              assert.ok(messages.includes('Detached @shopgate/cart'), 'should log detachment of extension')
+              resolve()
+            } catch (err) {
+              reject(err)
+            }
+          })
+        } catch (err) {
+          reject(err)
+        }
       })
-    }
-    ex(done)
+    })
   })
 })
